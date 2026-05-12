e1579ea7e12749b6811cafb3a338286cecf33517
beingprofessional123 <beingprofessional123@gmail.com>
2026-05-12 06:09:56 +0000

    update

--- status.php
+++ status.php
@@ -10,7 +10,5 @@ echo shell_exec('git status 2>&1');
 echo "\nLast Commit:\n";
 echo shell_exec('git log -1 --oneline 2>&1');
 
-echo "\nDisk:\n";
-echo shell_exec('df -h 2>&1');
-
 echo "</pre>";
+
